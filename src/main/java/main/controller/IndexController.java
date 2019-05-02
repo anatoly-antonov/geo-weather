f6888832bd34256b847513f131a71f9c321b68c0
Anatoly Antonov <anatolijs.antonovs@gmail.com>
2019-05-02 15:55:51 +0300

    Thymeleaf support & index page and controller

--- src/main/java/main/controller/IndexController.java
+++ src/main/java/main/controller/IndexController.java
@@ -1,14 +1,14 @@
 package main.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GeoController {
+@Controller
+public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-        return "test";
+        return "index";
     }
 }
 
